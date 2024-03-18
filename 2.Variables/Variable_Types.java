7148f1117582ac98cdff6a9454d79e36ea865876
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-18 23:21:02 +0530

    Add files via upload

---
+++ 2.Variables/Variable_Types.java
@@ -0,0 +1,21 @@
+package HelloWorld;
+
+public class Variable_Types {
+	static int num = 100; //static variable
+	
+	void method() {
+		int num1 = 200; //local variable
+		System.out.println("Num1: " + num1);
+	}
+	public static void main(String[] args) {
+		int num2 = 300; //instance variable
+		
+		System.out.println("Num: " + num);
+		
+		Variable_Types n = new Variable_Types();
+		n.method();
+		
+		System.out.println("Num2: " + num2);
+		
+	}
+}
