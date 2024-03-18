5e6d6824f085af045a28b8f54c749c13293435b9
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-18 22:55:57 +0530

    Add files via upload

---
+++ 2.Variables/Variables.java
@@ -0,0 +1,18 @@
+package HelloWorld;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		String name = "Ramu";
+		int age = 35;
+		float percent = 25.34f;
+		char gender = 'M';
+		boolean married = true;
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Percent: " + percent);
+		System.out.println("Gender: " + gender);
+		System.out.println("Married: " + married);
+	}
+
+}
