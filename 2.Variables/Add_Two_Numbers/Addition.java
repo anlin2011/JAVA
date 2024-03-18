b40850db4a0e09e18e2da607215296f3e7fc4036
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-18 21:05:39 +0530

    Add files via upload

---
+++ 2.Variables/Add_Two_Numbers/Addition.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1 = 45;
+		int num2 = 55;
+		int sum = num1 + num2;
+		System.out.println("Addition of two numbers: " + sum);
+	}
+
+}
