0e4324fa16c5779f4c47ee5baf3efbd903f637f1
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-19 13:17:51 +0530

    Add files via upload

---
+++ 2.Variables/Casting.java
@@ -0,0 +1,32 @@
+package HelloWorld;
+
+/*Widening Casting
+ 	byte -> short -> char -> int -> long -> float -> double
+  Narrowing Casting
+  	double -> float -> long -> int -> char -> short -> byte
+ */
+
+public class Casting {
+
+	public static void main(String[] args) {
+		int a = 10;
+		double b = a; //widening casting
+
+		double c = 25.5376;
+		int d = (int)c; //narrowing casting
+		
+		float e = 15.6f;
+		int f = (int)e;
+		
+		System.out.println("Widening Typecasting");
+		System.out.println("Integer:" + a);
+		System.out.println("Double:" + b);
+		
+		System.out.println("\nNarrowing Typecasting");
+		System.out.println("Double:" + c);
+		System.out.println("Integer:" + d);	
+		
+		System.out.println("\nFloat:" + e);
+		System.out.println("Integer:" + f);
+	}
+}
