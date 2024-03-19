da0d358ffb39e27573bd698397566a78d9e2db77
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-19 16:08:38 +0530

    Add files via upload

---
+++ 2.Variables/Datatypes.java
@@ -0,0 +1,22 @@
+package HelloWorld;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int num = 10000000;
+		float f1 = 2456.5f;
+		char grade = 'A';
+		char var = 67;
+		boolean isJavaFun = true;
+		String txt = "Welcome";
+		byte num1 = 125;
+		byte num2 = -128;
+		short num3 = 30000;
+		long num4 = 1567000000L;
+		double d1 = 345.34d;
+		System.out.println(num + "\n" + f1 + "\n" + grade + "\n" + var );
+		System.out.println(isJavaFun + "\n" + txt + "\n" + num1 + "\n" + num2 + "\n" + num3 + "\n" + num4 + "\n" + d1);
+		
+	}
+
+}
