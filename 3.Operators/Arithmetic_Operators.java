524f355255306def6a8fc6ea141ef44794ee7316
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-20 20:28:04 +0530

    Add files via upload

---
+++ 3.Operators/Arithmetic_Operators.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+public class Arithmetic_Operators {
+
+	public static void main(String[] args) {
+		int a = 183, b = 10;
+		//Arithmetic Operators
+		System.out.println("Addition:" + (a+b));
+		System.out.println("Subtraction:" + (a-b));
+		System.out.println("Multiplication: " + (a*b));
+		System.out.println("Division:" + (a/b));
+		System.out.println("Modulus:" + (a%b));
+		System.out.println(b*b/5+a-1*4/2); //100/5+183-4/2
+										   //20+183-2 = 203-2 = 201
+	}
+
+}
