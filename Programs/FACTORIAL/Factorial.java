a1d282c46fb336c4da23c61e3978a1e3b0eaf6de
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-21 22:34:20 +0530

    Add files via upload

---
+++ Programs/FACTORIAL/Factorial.java
@@ -0,0 +1,16 @@
+package Basic_Programs;
+import java.util.*;
+public class Factorial {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter n: ");
+	int n = sc.nextInt();
+	int fact = 1;
+	for(int i=1; i<=n; i++) {
+		fact *= i;
+	}
+	System.out.println("Factorial of " + n + " is: " + fact);
+	sc.close();
+	}
+}
