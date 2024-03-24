ffa775bcf4933be6d42b772b77d6b495f7c0ba57
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-24 20:47:44 +0530

    Add files via upload

---
+++ 3.Operators/Ternary_Operator.java
@@ -0,0 +1,18 @@
+package HelloWorld;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		int x = 20, y = 5;
+		int min = a<b?a:b;
+		int max = a>b?a:b;
+		int min1 = x<y?x:y;
+		int max1 = x>y?x:y;
+		System.out.println("Smallest number is: " + min);
+		System.out.println("Greatest number is: " + max);
+		System.out.println("Smallest number is: " + min1);
+		System.out.println("Greatest number is: " + max1);
+	}
+
+}
