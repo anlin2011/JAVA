635c08c5722357943db591acb1c3c6660c0f0d84
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-24 19:14:49 +0530

    Add files via upload

---
+++ 3.Operators/Relational_Operators.java
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+public class Relational_Operators {
+
+	public static void main(String[] args) {
+		int a = 100, b = 50;
+		System.out.println("Equal to: " + (a == b));
+		System.out.println("Not equal to: " + (a != b));
+		System.out.println("Greater than: " + (a > b));
+		System.out.println("Less than: " + (a < b));
+		System.out.println("Greater than or equal to: " + (a >= b));
+		System.out.println("Less than or equal to: " + (a <= b));
+	}
+
+}
