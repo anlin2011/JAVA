3927bad76637bf691b555817aeefc38967018cff
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-25 18:00:49 +0530

    Add files via upload

---
+++ 3.Operators/Assignment_Operators.java
@@ -0,0 +1,20 @@
+package HelloWorld;
+
+public class Assignment_Operators {
+
+	public static void main(String[] args) {
+		int a = 123;
+		System.out.println(a);
+		a += 100;
+		System.out.println(a);
+		a -= 10;
+		System.out.println(a);
+		a *= 10;
+		System.out.println(a);
+		a /= 10;
+		System.out.println(a);
+		a %= 10;
+		System.out.println(a);
+	}
+
+}
