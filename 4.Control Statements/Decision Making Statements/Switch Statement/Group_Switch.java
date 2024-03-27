72d3d68945e161e869e29a12b81cfaf8bfd929db
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-27 15:20:13 +0530

    Add files via upload

---
+++ 4.Control Statements/Decision Making Statements/Switch Statement/Group_Switch.java
@@ -0,0 +1,28 @@
+package Programs;
+import java.util.*;
+public class Group_Switch {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the character: ");
+		char choice = sc.next().charAt(0);
+		switch(choice) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println(choice + " is an Vowel");
+			break;
+		default:
+			System.out.println(choice + " is a Consonant");
+		}
+		sc.close();
+	}
+
+}
