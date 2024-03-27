69c0ccb7ecb48f80151a1f3496a119e31a46a8b9
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-27 15:15:19 +0530

    Add files via upload

---
+++ 4.Control Statements/Decision Making Statements/Switch Statement/Switch.java
@@ -0,0 +1,50 @@
+package Programs;
+import java.util.*;
+public class Switch {
+
+	public static void main(String[] args) {
+		System.out.println("Menu:");
+		System.out.println("1. Addition");
+		System.out.println("2. Subtraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+		int num1, num2, choice;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your choice: ");
+		choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			System.out.print("Enter number1: ");
+			num1 = sc.nextInt();
+			System.out.print("Enter number2: ");
+			num2 = sc.nextInt();
+			System.out.println("Addition: " + (num1 + num2));
+			break;
+		case 2:
+			System.out.print("Enter number1: ");
+			num1 = sc.nextInt();
+			System.out.print("Enter number2: ");
+			num2 = sc.nextInt();
+			System.out.println("Subtraction: " + (num1 - num2));
+			break;
+		case 3:
+			System.out.print("Enter number1: ");
+			num1 = sc.nextInt();
+			System.out.print("Enter number2: ");
+			num2 = sc.nextInt();
+			System.out.println("Multiplication: " + (num1 * num2));
+			break;
+		case 4:
+			System.out.print("Enter number1: ");
+			num1 = sc.nextInt();
+			System.out.print("Enter number2: ");
+			num2 = sc.nextInt();
+			System.out.println("Division: " + (num1 / num2));
+			break;
+		default:
+			System.out.println("Invalid Input!!!");		
+		}
+		sc.close();
+	}
+
+}
