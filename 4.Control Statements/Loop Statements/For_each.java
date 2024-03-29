501c8136d9767c577868639d1515ef0b8fea5458
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-29 17:14:11 +0530

    Add files via upload

---
+++ 4.Control Statements/Loop Statements/For_each.java
@@ -0,0 +1,11 @@
+package com.programs;
+
+public class For_each {
+
+	public static void main(String[] args) {
+		String[] languages = {"C", "C++", "Python", "Java", "Javascript"};
+		for(String lan:languages) {
+			System.out.println(lan);		}
+	}
+
+}
