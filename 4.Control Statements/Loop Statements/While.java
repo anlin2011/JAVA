51769f35b7facd937d9766fa1f5de805e41a2720
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-30 10:52:52 +0530

    Add files via upload

---
+++ 4.Control Statements/Loop Statements/While.java
@@ -0,0 +1,16 @@
+package Programs;
+import java.util.*;
+public class While {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n: ");
+		int n = sc.nextInt();
+		int i=1;
+		while(i<=n) {
+			System.out.println(i);
+			i++;
+		}
+		sc.close();
+	}
+}
