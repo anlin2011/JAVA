a42a759614ca26d7442f65e678e758ff542143ae
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-04-14 21:25:48 +0530

    Add files via upload

---
+++ 4.Control Statements/Loop Statements/Nested_For_Loop.java
@@ -0,0 +1,10 @@
+package com.programs;
+public class Nested_For_Loop {
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=4;j++) {
+				System.out.println(i + " " + j);
+			}
+		}
+	}
+}
