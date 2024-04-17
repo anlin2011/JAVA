7770501006e3cf34965b624f65af02c202807b28
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-04-17 13:07:50 +0530

    Add files via upload

---
+++ 4.Control Statements/Loop Statements/For_each2.java
@@ -0,0 +1,11 @@
+package com.programs;
+
+public class For_each2 {
+
+	public static void main(String[] args) {
+		int arr[] = {100, 200, 300, 400, 500};
+		for(int i:arr) {
+			System.out.println(i);
+		}
+	}
+}
