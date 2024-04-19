2c2a7c71ab7db8dfab66c6fc5b7f00ca4cd4bcc4
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-04-19 20:53:56 +0530

    Add files via upload

---
+++ 5.OOPS/1. Main within the class/Student.java
@@ -0,0 +1,13 @@
+package com.programs;
+
+class Student {
+	int id;
+	String name;
+	String email;
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+		System.out.println(s1.email);
+	}
+}
