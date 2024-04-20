92e11b95ec08c803273136ef1b9b95bd1179b96b
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-04-20 22:44:30 +0530

    Add files via upload

---
+++ 5.OOPS/2. Main outside the class/TestStudent1.java
@@ -0,0 +1,13 @@
+package com.programs;
+class Student1 {
+	int roll_no;
+	String name;
+}
+
+class TestStudent1 {
+	public static void main(String[] args) {
+		Student1 s1 = new Student1();
+		System.out.println(s1.roll_no);
+		System.out.println(s1.name);
+	}
+}
