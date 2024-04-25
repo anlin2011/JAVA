197908776cc2bfe53b755991372341423bc3f4df
Anlin Bernishia S <155153468+anlin2011@users.noreply.github.com>
2024-04-25 16:07:23 +0530

    Add files via upload

---
+++ 5.OOPS/7. Anonymous Object/Calculation.java
@@ -0,0 +1,14 @@
+package com.programs;
+
+class Calculation {
+	void fact(int n) {
+		int fact = 1;
+		for(int i=1; i<=n; i++) {
+			fact = fact * i;
+		}
+		System.out.println("Factorial of " + n + " is: " + fact);
+	}
+	public static void main(String[] args) {
+		new Calculation().fact(5);
+	}
+}
