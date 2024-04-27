e17569e45c84be33b917f1beb4298c153e263c17
Anlin Bernishia S <155153468+anlin2011@users.noreply.github.com>
2024-04-27 14:07:06 +0530

    Add files via upload

---
+++ 5.OOPS/9. Default Construtor/Bike.java
@@ -0,0 +1,12 @@
+package com.programs;
+
+class Bike {
+	Bike() {
+		//default constructor
+		System.out.println("Bike is created");
+	}
+	public static void main(String[] args) {
+		//calling a default constructor
+		new Bike();
+	}
+}
